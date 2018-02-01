377a97631a239dae7b06e9f512eacd2f4d637980
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-02-01 13:40:17 +0100

    DieselEngine

--- I4SWD/Lecture0-0/Interfaces-2/Vehicle.Application/Program.cs
+++ I4SWD/Lecture0-0/Interfaces-2/Vehicle.Application/Program.cs
@@ -12,14 +12,25 @@ namespace Vehicle.Application
         static void Main(string[] args)
         {
             var myGasEngine = new GasEngine(100);
+            var myDieselEngine = new DieselEngine(120);
+            var myMotorBike2 = new MotorBike(myDieselEngine);
             var myMotorBike = new MotorBike(myGasEngine);
 
             var thr = myGasEngine.GetThrottle();
             Console.WriteLine($"My throttle is: {thr}.");
 
+            var thr2 = myDieselEngine.GetThrottle();
+            Console.WriteLine($"My throttle is: {thr2}");
+
             myMotorBike.RunAtHalfSpeed();
             thr = myGasEngine.GetThrottle();
             Console.WriteLine($"My throttle at half speed is: {thr}");
+
+            myMotorBike2.RunAtHalfSpeed();
+            thr2 = myGasEngine.GetThrottle();
+            Console.WriteLine($"My throttle at half speed is: {thr2}");
+
+            Console.ReadLine();
         }
     }
 }
