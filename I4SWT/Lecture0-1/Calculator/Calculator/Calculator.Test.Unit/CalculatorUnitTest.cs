bfc099c40a1875606bc719c75d1c3168bc56189e
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-02-01 17:52:08 +0100

    TestCases added for everything -exceptions

--- I4SWT/Lecture0-1/Calculator/Calculator/Calculator.Test.Unit/CalculatorUnitTest.cs
+++ I4SWT/Lecture0-1/Calculator/Calculator/Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -8,58 +8,127 @@ using NUnit.Framework;
 namespace Calculator.Test.Unit
 {
     [TestFixture()]
-    public class CalculatorUnitTests
+    [Author("Gruppe 35")]
+    public class CalculatorUnitTest
     {
-        [Test]
-        public void Add_Add2And4_Returns6()
-        {
-            var uut = new Calculator();
+        private Calculator _uut;
 
-            Assert.That(uut.Add(2, 4), Is.EqualTo(6));
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Calculator();
         }
 
-        [Test]
-        public void Add_AddMinus2And4_Returns2()
+        [TestCase(2, 4, 6)]
+        [TestCase(-4, 8, 4)]
+        [TestCase(3, -7, -4)]
+        public void Add_AddNumber1AndNumber2_ReturnsCorrectResult(double a, double b, double c)
         {
-            var uut = new Calculator();
-
-            Assert.That(uut.Add(-2, 4), Is.EqualTo(2));
+            Assert.That(_uut.Add(a, b), Is.EqualTo(c));
         }
 
-        [Test]
-        public void Subtract_Subtract5And2_Returns3()
+        [TestCase(1, 2, 3, 6)]
+        [TestCase(-4, 2, 5, 3)]
+        [TestCase(5, -7, -1, -3)]
+        public void Add_Add_AddNumber1AndNumber2AndNumber3_ReturnsCorrectResult(double a, double b, double c, double d)
         {
-            var uut = new Calculator();
-
-            Assert.That(uut.Subtract(5, 2), Is.EqualTo(3));
+            _uut.Add(a, b);
+            Assert.That(_uut.Add(c), Is.EqualTo(d));
         }
 
-        [Test]
-        public void Multiply_Multiply3And4_Returns12()
+        [TestCase(2, 4, -2)]
+        [TestCase(0, 4, -4)]
+        [TestCase(4, 2, 2)]
+        [TestCase(-3, -3, 0)]
+        public void Subtract_SubtractNumber1AndNumber2_ReturnCorrectResult(double a, double b, double c)
         {
-            var uut = new Calculator();
-
-            Assert.That(uut.Multiply(3, 4), Is.EqualTo(12));
+            Assert.That(_uut.Subtract(a, b), Is.EqualTo(c));
         }
 
-        [Test]
-        public void Power_Power3And3_Returns27()
+        [TestCase(4, 1, 3, 0)]
+        [TestCase(-6, -1, 4, -9)]
+        public void Subtract_Subtract_SubtractNumber2AndNumber3FromNumber1_ReturnCorrectResult(double a,
+            double b, double c, double d)
         {
-            var uut = new Calculator();
+            _uut.Subtract(a, b);
+            Assert.That(_uut.Subtract(c), Is.EqualTo(d));
+        }
 
-            Assert.That(uut.Power(3, 3), Is.EqualTo(27));
+        [TestCase(3, 4, 12)]
+        [TestCase(0, 5, 0)]
+        [TestCase(2, -5, -10)]
+        [TestCase(1, 1, 1)]
+        public void Multiply_MultiplyNumber1AndNumber2_ReturnCorrectResult(double a, double b, double c)
+        {
+            Assert.That(_uut.Multiply(a, b), Is.EqualTo(c));
+        }
+
+        [TestCase(2, 2, 2, 8)]
+        [TestCase(4, -2, 3, -24)]
+        [TestCase(0, 3, 2, 0)]
+        [TestCase(4, -2, -1, 8)]
+        public void Multiply_Multiply_MultiplyNumber1AndNumber2AndNumber3_ReturnCorrectResult(double a, double b,
+            double c, double d)
+        {
+            _uut.Multiply(a, b);
+            Assert.That(_uut.Multiply(c), Is.EqualTo(d));
         }
 
         [TestCase(3, 3, 27)]
-        [TestCase(5, 0, 1)]
-        [TestCase(0, 5, 0)]
-        [TestCase(5, -1, 0.2)]
+        [TestCase(1, 0, 1)]
+        [TestCase(0, 2, 0)]
         [TestCase(-2, 3, -8)]
-        public void Power_Test(double a, double b, double c)
+        [TestCase(5, -1, 0.2)]
+        public void Power_Number1TakenToThePowerOfNumber2_ReturnCorrectResult(double a, double b, double c)
         {
-            var uut = new Calculator();
+            Assert.That(_uut.Power(a, b), Is.EqualTo(c));
+        }
 
-            Assert.That(uut.Power(a,b), Is.EqualTo(c));
+        [TestCase(2, 2, 3, 64)]
+        [TestCase(0, 2, 3, 0)]
+        [TestCase(3, 2, 0, 1)]
+        [TestCase(-5, 2, 2, 625)]
+        [TestCase(2, 2, -1, 0.25)]
+        public void Power_Power_Number1TakenToThePowerOfNumber2TakenToThePowerOfNumber3_ReturnCorrectResult(double a,
+            double b, double c, double d)
+        {
+            _uut.Power(a, b);
+            Assert.That(_uut.Power(c), Is.EqualTo(d));
+        }
+
+        [TestCase(6, 2, 3)]
+        [TestCase(-4, 2, -2)]
+        [TestCase(8, -2, -4)]
+        // [TestCase(6, 0, 99999)] <- 99999 should be infinity, or test for exception thrown.
+        [TestCase(0, 10, 0)]
+        public void Divide_Number1DividedByNumber2_ReturnCorrectResult(double a, double b, double c)
+        {
+            Assert.That(_uut.Divide(a, b), Is.EqualTo(c));
+        }
+
+        [TestCase(8, 2, 1, 4)]
+        [TestCase(0, 2, 2, 0)]
+        // [TestCase(6, 2, 0, 99999)] <- 99999 should be infinity, or test for exception thrown.
+        [TestCase(-8, 2, 2, -2)]
+        [TestCase(-12, 2, -3, 2)]
+        public void Divide_Divide_Number1DividedByNumber2DividedByNumber3_ReturnCorrectResult(double a, double b,
+            double c, double d)
+        {
+            _uut.Divide(a, b);
+            Assert.That(_uut.Divide(c), Is.EqualTo(d));
+        }
+
+        [TestCase(8, 2, 4, -4, -16, 14, -2, -1, -0.5)]
+        [TestCase(-12, 3, -4, 10, -40, -10, -50, 3, -125000)]
+        // [TestCase(4, 0, 99999, -1, -99999, 14, -99999, 1, -99999)] <- 99999 should be infinity, and -99999 should be negativve infinity, or exception thrown.
+        public void Add_Divide_Multiply_Add_Power_AddNumber1DivideByNumber2MultiplyByNumber4AddNumber6TakenToThePowerOfNumber8_ReturnCorrectResult (double a, 
+            double b, double c, double d, double e, double f, double g, double h, double i)
+        {
+            Assert.That(_uut.Add(a), Is.EqualTo(a));
+            Assert.That(_uut.Divide(b), Is.EqualTo(c));
+            Assert.That(_uut.Multiply(d), Is.EqualTo(e));
+            Assert.That(_uut.Add(f), Is.EqualTo(g));
+            Assert.That(_uut.Power(h), Is.EqualTo(i));
         }
     }
 }
--- I4SWT/Lecture0-1/Calculator/Calculator/Calculator/Calculator.cs
+++ I4SWT/Lecture0-1/Calculator/Calculator/Calculator/Calculator.cs
@@ -10,24 +10,96 @@ namespace Calculator
     {
         public double Add(double a, double b)
         {
-            return a + b;
+            var result = a + b;
+            Accumulator = result;
+            return result;
+        }
+
+        public double Add(double addend)
+        {
+            var result = addend + Accumulator;
+            Accumulator = result;
+            return result;
         }
 
         public double Subtract(double a, double b)
         {
-            return a - b;
+            var result = a - b;
+            Accumulator = result;
+            return result;
         }
 
+        public double Subtract(double subtractor)
+        {
+            var result = Accumulator - subtractor;
+            Accumulator = result;
+            return result;
+        }
         public double Multiply(double a, double b)
         {
-            return a * b;
+            var result = a * b;
+            Accumulator = result;
+            return result;
+        }
+
+        public double Multiply(double multiplier)
+        {
+            var result = Accumulator * multiplier;
+            Accumulator = result;
+            return result;
         }
 
         public double Power(double a, double b)
         {
-            return Math.Pow(a, b);
+            var result = Math.Pow(a, b);
+            Accumulator = result;
+            return result;
         }
 
+        public double Power(double exponent)
+        {
+            var result = Math.Pow(Accumulator, exponent);
+            Accumulator = result;
+            return result;
+        }
+
+        public double Divide(double dividend, double divisor)
+        {
+            try
+            {
+                var result = dividend / divisor;
+                Accumulator = result;
+                return result;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"The dividend {dividend} is being divided by zero");
+                Accumulator = 0;
+                return 0;
+            }
+        }
+
+        public double Divide(double divisor)
+        {
+            try
+            {
+                var result = Accumulator / divisor;
+                Accumulator = result;
+                return result;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"The dividend {Accumulator} is being divided by zero");
+                throw;
+            }
+        }
+
+        public double Accumulator { get; private set; }
+
+        public void Clear()
+        {
+            Accumulator = 0;
+        }
 
     }
 }
