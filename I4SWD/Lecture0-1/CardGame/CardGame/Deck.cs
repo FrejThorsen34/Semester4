8405b118f09e886e957c3c3b8c0a4ca7e3b47ec1
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-02-02 15:43:36 +0100

    Cardgame +deck +iplayer +highroller

---
+++ I4SWD/Lecture0-1/CardGame/CardGame/Deck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame
+{
+    public class Deck
+    {
+        private ICard _card = null;
+
+        public Deck(ICard card)
+        {
+            _card = card;
+        }
+        public void DealCards(IPlayer player, int number)
+        {
+
+        }
+    }
+}
---
+++ I4SWD/Lecture0-1/CardGame/CardGame/Highroller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame
+{
+    public class Highroller : IPlayer
+    {
+        private string _name;
+        private 
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+
+    }
+}
