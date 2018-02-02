dc09716641946e304af8c68aa3f3d2ab2fa2dbfc
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-02-02 10:30:43 +0100

    Simplied get; set; for CurThrottle

--- I4SWD/Lecture0-0/Interfaces-2/Vehicles/DieselEngine.cs
+++ I4SWD/Lecture0-0/Interfaces-2/Vehicles/DieselEngine.cs
@@ -8,23 +8,13 @@ namespace Vehicles
 {
     public class DieselEngine : IEngine
     {
-        private uint _curThrottle = 0;
-        private uint _maxThrottle = 0;
-
         public DieselEngine(uint maxThrottle)
         {
-            _maxThrottle = maxThrottle;
+            MaxThrottle = maxThrottle;
         }
 
-        public uint MaxThrottle
-        {
-            get { return _maxThrottle; }
-        }
+        public uint MaxThrottle { get; }
 
-        public uint CurThrottle
-        {
-            get { return _curThrottle; }
-            set { _curThrottle = value; }
-        }
+        public uint CurThrottle { get; set; }
     }
 }
