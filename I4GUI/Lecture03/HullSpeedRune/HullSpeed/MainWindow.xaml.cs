11c3bbed2dfa2089b0578e38b11cb1e757eb50d3
Rune Aagaard Keena <runek@au.dk>
2018-02-05 13:07:26 +0100

    HullSpeedRune

---
+++ I4GUI/Lecture03/HullSpeedRune/HullSpeed/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HullSpeed
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		SailBoat boat;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			boat = new SailBoat();
+			Loaded += new RoutedEventHandler(MainWindow_Loaded);
+		}
+
+		private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			Keyboard.Focus(tbxName);
+		}
+
+		private void tbxName_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			boat.Name = tbxName.Text;
+		}
+
+		private void tbxLength_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			boat.Length = Double.Parse(tbxLength.Text);
+		}
+
+		private void btnCalculateHullSpeed_Click(object sender, RoutedEventArgs e)
+		{
+			tbxHullSpeed.Text = boat.Hullspeed().ToString("F2");
+		}
+	}
+}
---
+++ I4GUI/Lecture03/HullSpeedRune/HullSpeed/SailBoat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HullSpeed
+{
+	class SailBoat
+	{
+		public string Name { get; set; }
+		
+		public double Length { get; set; }
+
+		public double Hullspeed()
+		{
+			return 1.34 * System.Math.Sqrt(Length);
+		}
+	}
+}
