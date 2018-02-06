372305f5a8d2012bdcccc6cce03fd49dba280f08
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-02-06 19:09:24 +0100

    GUI Lecture04

---
+++ I4GUI/Lecture04/HullSpeed2Nickolai/HullSpeed2Nickolai-2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HullSpeed2Nickolai_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Sailboat boat;
+        public MainWindow()
+        {
+            InitializeComponent();
+            boat = new Sailboat();
+            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Keyboard.Focus(tbxName);
+        }
+
+        private void tbxLength_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            boat.Length = Double.Parse(tbxLength.Text);
+        }
+        private void tbxName_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            boat.Name = tbxName.Text;
+        }
+
+        private void BtnCalculateHullSpeed_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ I4GUI/Lecture04/HullSpeed2Nickolai/HullSpeed2Nickolai/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HullSpeed2Nickolai
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Sailboat boat;
+        public MainWindow()
+        {
+            InitializeComponent();
+            boat = new Sailboat();
+            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Keyboard.Focus(tbxName);
+        }
+        private void BtnCalculateHullSpeed_OnClick(object sender, RoutedEventArgs e)
+        {
+            tbxHullSpeed.Text = boat.Hullspeed().ToString("F1");
+        }
+
+        private void tbxLength_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            boat.Length = Double.Parse(tbxLength.Text);
+        }
+        private void tbxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            boat.Name = tbxName.Text;
+        }
+    }
+}
