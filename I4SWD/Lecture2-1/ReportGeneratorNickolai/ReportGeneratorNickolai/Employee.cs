4726c8401373de600583abbd3a68804585578950
poxbabs <nickolai.ernst@hotmail.com>
2018-02-09 10:44:23 +0100

    Employee readded

---
+++ I4SWD/Lecture2-1/ReportGeneratorNickolai/ReportGeneratorNickolai/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReportGeneratorNickolai
+{
+    public class Employee
+    {
+        public Employee() : this("", 0)
+        {
+        }
+
+        public Employee(string name, uint salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        public string Name { get; private set; }
+        public uint Salary { get; private set; }
+    }
+}
