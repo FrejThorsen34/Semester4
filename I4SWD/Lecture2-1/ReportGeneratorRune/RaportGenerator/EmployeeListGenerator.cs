bae5ab0f2e189e6e904372988a8e7a3fb6757c0f
Rune Aagaard Keena <runekeena@gmail.com>
2018-02-09 10:39:26 +0100

    RaportGeneratorRune

---
+++ I4SWD/Lecture2-1/ReportGeneratorRune/RaportGenerator/EmployeeListGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReportGenerator;
+
+namespace RaportGenerator
+{
+	//not needed
+
+	public class EmployeeListGenerator
+	{
+		public List<Employee> GenerateEmployeeList(EmployeeDB employeeDb)
+		{
+			var employees = new List<Employee>();
+			Employee employee;
+
+			employeeDb.Reset();
+
+			// Get all employees
+			while ((employee = employeeDb.GetNextEmployee()) != null)
+			{
+				employees.Add(employee);
+			}
+
+			return employees;
+		}
+		
+}
+}
