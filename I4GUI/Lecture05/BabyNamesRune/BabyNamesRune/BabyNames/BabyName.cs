5b5a77c31c2a83270cef8796b286dd32f38659e1
Rune <runekeena@gmail.com>
2018-02-19 13:30:12 +0100

    GUI - BabyNames - Rune added

---
+++ I4GUI/Lecture05/BabyNamesRune/BabyNamesRune/BabyNames/BabyName.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyNames
+{
+	class BabyName
+	{
+		private string _name;
+		private int[] _ranks;
+
+		public string Name
+		{
+			get { return _name; }
+		}
+
+		public BabyName(string nameAndRankings)
+		{
+			string[] tokens;
+			char[] seperators = {' '};
+
+			tokens = nameAndRankings.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+			this._name = tokens[0];
+			this._ranks = new int[10];
+			for (int i = 0; i < 10; i++)
+			{
+				this._ranks[i] = int.Parse(tokens[i + 1]);
+			}
+		}
+
+		public int Rank(int year)
+		{
+			int index = (year - 1900) / 10;
+			if (index < 0 || index > 10)
+			{
+				throw new SystemException("Year must be 1900, 1910 ... 2000");
+			}
+
+			return this._ranks[index];
+		}
+
+		public int AverageRank()
+		{
+			int sum = 0;
+
+			for (int i = 0; i < this._ranks.Length; i++)
+			{
+				if (this._ranks[i] == 0)
+					sum += 2500;
+				else
+					sum += this._ranks[i];
+			}
+
+			return sum / this._ranks.Length;
+		}
+	}
+}
---
+++ I4GUI/Lecture05/BabyNamesRune/BabyNamesRune/BabyNames/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BabyNames
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private List<BabyName> _namesCollection;
+		private string[,] _rankMatrix = new string[10, 10];
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			Loaded += new RoutedEventHandler(MainWindow_Loaded);
+			lstDecade.SelectionChanged += new SelectionChangedEventHandler(lstDecade_SelectionChanged);
+		}
+
+		private void lstDecade_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			ListBoxItem item = lstDecade.SelectedItem as ListBoxItem;
+			if (item != null)
+			{
+				int deade = (Convert.ToInt32(item.Content) - 1900) / 10;
+				
+			}
+		}
+
+		void MainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			string file = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "babynames.txt");
+			this._namesCollection = LoadData.ReadBabyNameData(file);
+
+			foreach (BabyName name in _namesCollection)
+			{
+				for (int decade = 1900; decade < 2010; decade += 10)
+				{
+					int rank = name.Rank(decade);
+					int decadeIndex = (decade - 1900) / 10;
+					if(rank>0 && rank<11)
+						if (_rankMatrix[decadeIndex, rank - 1] == null)
+						{
+							_rankMatrix[decadeIndex, rank - 1] = name.Name;
+						}
+						else
+						{
+							_rankMatrix[decadeIndex, rank-1] += "and" + name.Name;
+						}
+
+				}
+			}
+		}
+	}
+}
