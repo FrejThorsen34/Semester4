0fc6af2c3e9e9e51a7d3679094d61fb92fb39967
Rune <runekeena@gmail.com>
2018-02-27 15:12:54 +0100

    DoorControl

---
+++ I4SWT/Lecture4-2/DoorControl/DoorControl.Test.Unit/DoorControlUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DoorControl.Test.Unit
+{
+	[TestFixture]
+	[Author("Gruppe 35")]
+    public class DoorControlUnitTest
+	{
+		private DoorControl _uut;
+		private IDoor _door = Substitute.For<IDoor>();
+		private IUserValidation _userValidation = Substitute.For<IUserValidation>();
+		private IEntryNotification _entryNotification = Substitute.For<IEntryNotification>();
+
+		[SetUp]
+		public void Setup()
+		{
+			_uut = new DoorControl(_door, _userValidation, _entryNotification);
+			_userValidation.ValidateEntryRequest(0).Returns(false);
+			_userValidation.ValidateEntryRequest(1).Returns(true);
+		}
+
+		[Test]
+		public void OpenDoor_Test()
+		{
+			
+			_uut.RequestEntry(1);
+			_userValidation.Received().ValidateEntryRequest(1);
+			_entryNotification.Received().NotifyEntryGranted();
+		}
+	}
+}
---
+++ I4SWT/Lecture4-2/DoorControl/DoorControl/DoorControl.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorControl
+{
+	public class DoorControl
+	{
+		private enum DoorState
+		{
+			DoorClosed,
+			DoorOpening,
+			DoorClosing,
+			DoorBreached
+		}
+
+		private DoorState _state;
+		private IDoor _door;
+		private IUserValidation _userValidation;
+		private IEntryNotification _entryNotification;
+
+		public DoorControl(IDoor door, IUserValidation userValidation, IEntryNotification entryNotification)
+		{
+			this._door = door;
+			this._userValidation = userValidation;
+			this._entryNotification = entryNotification;
+			_state = DoorState.DoorClosed;
+		}
+
+		public void RequestEntry(int id)
+		{
+			switch (_state)
+			{
+				case DoorState.DoorClosed:
+					bool idOk = _userValidation.ValidateEntryRequest(id);
+					if (idOk)
+					{
+						_door.Open();
+						_entryNotification.NotifyEntryGranted();
+						_state = DoorState.DoorOpening;
+					}
+					else
+					{
+						_entryNotification.NotifyEntryDenied();
+					}
+					break;
+				default:
+					break;
+			}
+		}
+
+		public void DoorOpened()
+		{
+			switch (_state)
+			{
+				case DoorState.DoorOpening:
+					_door.Close();
+					_state = DoorState.DoorClosing;
+					break;
+				case DoorState.DoorClosed:
+					_door.Close();
+					_entryNotification.SignalAlarm();
+					_state = DoorState.DoorBreached;
+					break;
+				default:
+					break;
+			}
+		}
+
+		public void DoorClosed()
+		{
+			switch (_state)
+			{
+				case DoorState.DoorClosing:
+					_state = DoorState.DoorClosed;
+					break;
+				default:
+					break;
+			}
+		}
+
+	}
+}
