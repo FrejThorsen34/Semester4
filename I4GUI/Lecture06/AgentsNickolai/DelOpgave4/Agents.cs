4eaf2d3ba0a1b5c1fff5ee9e019cdf30fe6a82ec
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-02-28 12:19:54 +0100

    Almost done

---
+++ I4GUI/Lecture06/AgentsNickolai/DelOpgave4/Agents.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmFoundation.Wpf;
+using System.Windows.Input;
+using System.Runtime.CompilerServices;
+
+namespace DelOpgave4
+{
+    public class Agents : ObservableCollection<Agent>, INotifyPropertyChanged
+    {
+        public Agents()
+        {
+            Add(new Agent("001", "Nina", "Assassination", "UpperVolta"));
+            Add(new Agent("007", "James Bond", "Martinis", "North Korea"));
+        }
+
+        #region Commands
+
+        ICommand _addCommand;
+        public ICommand AddCommand
+        {
+            get { return _addCommand ?? (_addCommand = new RelayCommand(AddAgent)); }
+        }
+
+        private void AddAgent()
+        {
+            Add(new Agent());
+            CurrentIndex = Count - 1;
+        }
+
+        ICommand _deleteCommand;
+
+        public ICommand DeleteCommand
+        {
+            get { return _deleteCommand ?? (_deleteCommand = new RelayCommand(DeleteAgent, DeleteAgent_CanExecute)); }
+        }
+
+        private void DeleteAgent()
+        {
+            RemoveAt(CurrentIndex);
+        }
+
+        private bool DeleteAgent_CanExecute()
+        {
+            if (Count > 0 && CurrentIndex >= 0)
+                return true;
+            else
+                return false;
+        }
+
+        ICommand _nextCommand;
+        public ICommand NextCommand
+        {
+            get
+            {
+                return _nextCommand ?? (_nextCommand = new RelayCommand(
+                           () => ++CurrentIndex,
+                           () => CurrentIndex < (Count - 1)));
+            }
+        }
+
+        ICommand _previousCommand;
+        public ICommand PreviousCommand
+        {
+            get
+            {
+                return _previousCommand ??
+                       (_previousCommand = new RelayCommand(PreviousCommandExecute, PreviousCommand_CanExecute));
+            }
+        }
+
+        private void PreviousCommandExecute()
+        {
+            if (CurrentIndex > 0)
+                --CurrentIndex;
+        }
+
+        private bool PreviousCommand_CanExecute()
+        {
+            if (CurrentIndex > 0)
+                return true;
+            else
+                return false;
+        }
+        #endregion // Commands
+
+        #region Properties
+
+        int currentIndex = -1;
+
+        public int CurrentIndex
+        {
+            get { return currentIndex; }
+            set
+            {
+                if (currentIndex != value)
+                {
+                    currentIndex = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        #endregion // Properties
+
+        #region INotifyPropertyChanged implementation
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
