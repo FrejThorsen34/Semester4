4fd2d932975bd4dc4043a945cbc96ee164a951b8
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-04-05 19:17:56 +0200

    measurement_server done

--- I4IKN/Exercise7/Exercise7/measurement_server/measurement_server.cs
+++ I4IKN/Exercise7/Exercise7/measurement_server/measurement_server.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,86 @@ namespace measurement_server
 {
     public class measurement_server
     {
+        /// <summary>
+        /// The PORT
+        /// </summary>
+        const int PORT = 9000;
+
+        private measurement_server()
+        {
+            // Create local IPEndPoint, UdpClient and Socket
+            IPEndPoint endPointLocal = new IPEndPoint(IPAddress.Any, PORT);
+            UdpClient server_udp = new UdpClient(endPointLocal);
+            Socket server_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            string received;
+            byte[] send;
+
+            while (true)
+            {
+                Console.WriteLine("Listening...");
+
+                received = Encoding.ASCII.GetString(server_udp.Receive(ref endPointLocal));
+                IPEndPoint endPointSend = new IPEndPoint(endPointLocal.Address, PORT);
+
+                Console.WriteLine("Connected!");
+
+                switch (received)
+                {
+                    case "u":
+                    case "U":
+                        send = Encoding.ASCII.GetBytes(File.ReadAllText("/proc/uptime"));
+                        try
+                        {
+                            Console.WriteLine("Sending uptime.");
+                            server_socket.SendTo(send, endPointSend);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                            throw;
+                        }
+
+                        break;
+                    case "l":
+                    case "L":
+                        send = Encoding.ASCII.GetBytes(File.ReadAllText("/proc/loadavg"));
+                        try
+                        {
+                            Console.WriteLine("Sending loadavg.");
+                            server_socket.SendTo(send, endPointSend);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                            throw;
+                        }
+
+                        break;
+                    default:
+                        send = Encoding.ASCII.GetBytes("Invalid");
+                        Console.WriteLine("Invalid request.");
+                        try
+                        {
+                            server_socket.SendTo(send, endPointSend);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                            throw;
+                        }
+
+                        break;
+                }
+
+                Console.WriteLine("Handling complete");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("UDP Server starting.");
+            new measurement_server();
+        }
     }
 }
