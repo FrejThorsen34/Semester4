3b3649479a19926f3bf3d48dd6134cfe2d144a32
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-04-05 19:01:09 +0200

    Initial commit

---
+++ I4IKN/Exercise7/Exercise7/measurement_server/measurement_server.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace measurement_server
+{
+    public class measurement_server
+    {
+    }
+}
