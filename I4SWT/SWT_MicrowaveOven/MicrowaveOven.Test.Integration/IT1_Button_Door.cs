277df833490cd7424eba8a38a43449b11254d710
poxbabs <nickolai.ernst@hotmail.com>
2018-04-11 13:44:18 +0200

    Integrationtests added

---
+++ I4SWT/SWT_MicrowaveOven/MicrowaveOven.Test.Integration/IT1_Button_Door.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using MicrowaveOvenClasses.Boundary;
+
+namespace MicrowaveOven.Test.Integration
+{
+    [TestFixture]
+    public class IT1_Button_Door
+    {
+        //Stubs
+        private IDisplay _display;
+        private ILight _light;
+        private ITimer _timer;
+        private IPowerTube _powerTube;
+        private ICookController _cookController;
+        private IUserInterface _userInterface;
+
+        //Units under test
+        private IButton _uut_powerButton;
+        private IButton _uut_timeButton;
+        private IButton _uut_startCancelButton;
+        private IDoor _uut_door;
+
+        [SetUp]
+        public void SetUp()
+        {
+            //Stubs
+            _display = Substitute.For<IDisplay>();
+            _light = Substitute.For<ILight>();
+            _timer = Substitute.For<ITimer>();
+            _powerTube = Substitute.For<IPowerTube>();
+            _cookController = Substitute.For<ICookController>();
+            _userInterface = Substitute.For<IUserInterface>();
+
+            //Uut setup
+            _uut_powerButton = new Button();
+            _uut_timeButton = new Button();
+            _uut_startCancelButton = new Button();
+            _uut_door = new Door();
+        }
+
+        [Test]
+        public void Door_Open_UserInterface_OnDoorOpened()
+        {
+            _uut_door.Opened += (sender, args) => _userInterface.OnDoorOpened(_uut_door, EventArgs.Empty);
+            _uut_door.Open();
+            _userInterface.Received(1).OnDoorOpened(_uut_door, EventArgs.Empty);
+        }
+
+        [Test]
+        public void Door_Close__UserInterface_OnDoorlosed()
+        {
+            _uut_door.Closed += (sender, args) => _userInterface.OnDoorClosed(_uut_door, EventArgs.Empty);
+            _uut_door.Close();
+            _userInterface.Received(1).OnDoorClosed(_uut_door, EventArgs.Empty);
+        }
+
+        [Test]
+        public void PowerButton_Pressed__UserInterface_OnPowerPressed()
+        {
+            _uut_powerButton.Pressed += (sender, args) => _userInterface.OnPowerPressed(_uut_powerButton, EventArgs.Empty);
+            _uut_powerButton.Press();
+            _userInterface.Received(1).OnPowerPressed(_uut_powerButton, EventArgs.Empty);
+        }
+
+        [Test]
+        public void TimeButton_Pressed__UserInterface_OnTimePressed()
+        {
+            _uut_timeButton.Pressed += (sender, args) => _userInterface.OnTimePressed(_uut_timeButton, EventArgs.Empty);
+            _uut_timeButton.Press();
+            _userInterface.Received(1).OnTimePressed(_uut_timeButton, EventArgs.Empty);
+        }
+
+        [Test]
+        public void StartCancelButton_Pressed__UserInterface_OnStartCancelPressed()
+        {
+            _uut_startCancelButton.Pressed += (sender, args) => _userInterface.OnStartCancelPressed(_uut_startCancelButton, EventArgs.Empty);
+            _uut_startCancelButton.Press();
+            _userInterface.Received(1).OnStartCancelPressed(_uut_startCancelButton, EventArgs.Empty);
+        }
+    }
+}
---
+++ I4SWT/SWT_MicrowaveOven/MicrowaveOven.Test.Integration/IT4_Timer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using NSubstitute;
+using NUnit.Framework;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using System.Threading;
+using Timer = MicrowaveOvenClasses.Boundary.Timer;
+
+namespace MicrowaveOven.Test.Integration
+{
+    class IT4_Timer
+    {
+        //Stubs
+        private IOutput _output;
+
+
+        //Real
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _startCancelButton;
+        private IDoor _door;
+        private IUserInterface _userInterface;
+        private CookController _cookController;
+        private IDisplay _display;
+        private ILight _light;
+        private IPowerTube _powerTube;
+
+        //UUT
+        private ITimer _uut_timer;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            //Stubs
+            _output = Substitute.For<IOutput>();
+
+            //UUT
+            _uut_timer = new Timer();
+
+            //Real
+            _display = new Display(_output);
+            _light = new Light(_output);
+            _powerTube = new PowerTube(_output);
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _startCancelButton = new Button();
+            _door = new Door();
+            _cookController = new CookController(_uut_timer, _display, _powerTube);
+            _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
+            _cookController.UI = _userInterface;
+        }
+
+        [Test]
+        public void Timer_TimeTick_OutputShowsTime()
+        {
+
+            ManualResetEvent pause = new ManualResetEvent(false);
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
+            pause.WaitOne(2100);
+            _output.Received(1).OutputLine("Display shows: 00:58");
+        }
+
+        [Test]
+        public void Timer_FinishCooking_OutputLightOff()
+        {
+
+            ManualResetEvent pause = new ManualResetEvent(false);
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
+            pause.WaitOne(61000);
+            _output.Received(1).OutputLine("Light is turned off");
+            _output.Received(1).OutputLine("PowerTube turned off");
+        }
+    }
+}
