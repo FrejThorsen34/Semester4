31acec5b46f2ad0485a7fa63fe16002d73a15f2f
Rune <runekeena@gmail.com>
2018-04-13 13:46:53 +0200

    -

--- I4SWD/HandIn3/ChainOfResponsibility/ChainOfResponsibility.Application/Program.cs
+++ I4SWD/HandIn3/ChainOfResponsibility/ChainOfResponsibility.Application/Program.cs
@@ -16,26 +16,14 @@ namespace ChainOfResponsibility.Application
 		    Request testRequest3 = new Request(RequestType.Haste, "HasteRequest");
             Request testRequest4 = new Request(RequestType.Critical, "CriticalRequest");
 		    Request testRequest5 = new Request(RequestType.Overdue, "OverdueRequest");
-
-            Handler regularHandler = new RegularHandler();
-			Handler urgentHandler = new UrgentHandler();
-		    Handler hasteHandler = new HasteHandler();
-            Handler criticalHandler = new CriticalHandler();
-		    Handler overdueHandler = new OverdueHandler();
-
-            regularHandler.SetNextHandler(urgentHandler);
-			urgentHandler.SetNextHandler(hasteHandler);
-		    hasteHandler.SetNextHandler(criticalHandler);
-            criticalHandler.SetNextHandler(overdueHandler);
+			Request testRequest6 = new Request(RequestType.Other, "OtherRequest");
 			
-            regularHandler.HandleRequest(testRequest1);
-		    regularHandler.HandleRequest(testRequest2);
-		    regularHandler.HandleRequest(testRequest3);
-		    regularHandler.HandleRequest(testRequest4);
-		    regularHandler.HandleRequest(testRequest5);
-			
-			var testRequest6 = new Request(RequestType.Other, "OtherRequest");
 			var client = new Client();
+			client.HandleRequest(testRequest1);
+			client.HandleRequest(testRequest2);
+			client.HandleRequest(testRequest3);
+			client.HandleRequest(testRequest4);
+			client.HandleRequest(testRequest5);
 			client.HandleRequest(testRequest6);
 
 			Console.ReadKey();
