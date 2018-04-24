bf6f8dd13f236cd0691f75ba95b730a11ec5bdf7
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-04-24 09:55:08 +0200

    Now utilizing UOW and REPO in DODB

--- I4DAB/Handin3Rune/F18I4DABH2Gr30/F18IDABH2Gr30CosmosDB/Program.cs
+++ I4DAB/Handin3Rune/F18I4DABH2Gr30/F18IDABH2Gr30CosmosDB/Program.cs
@@ -5,267 +5,183 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Net;
+using DocumentDB.Repository;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Client.TransientFaultHandling;
 using Newtonsoft.Json;
 
 namespace F18IDABH2Gr30CosmosDB
 {
-	class Program
-	{
-		private const string EndpointUrl = "https://localhost:8081";
+    class Program
+    {
+        private const string EndpointUrl = "https://localhost:8081";
 
-		private const string PrimaryKey =
-			"C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        private const string PrimaryKey =
+            "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
-		private DocumentClient client;
+        private const string databaseId = "PersonKartotekDB";
 
-		static void Main(string[] args)
-		{
-			try
-			{
-				Program p = new Program();
-				p.GetStartedDemo().Wait();
-			}
-			catch (DocumentClientException de)
-			{
-				Exception baseException = de.GetBaseException();
-				Console.WriteLine("{0} error occurred: {1}, Message: {2}", de.StatusCode, de.Message, baseException.Message);
-			}
-			catch (Exception e)
-			{
-				Exception baseException = e.GetBaseException();
-				Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
-			}
-			finally
-			{
-				Console.WriteLine("End of demo, press any key to exit.");
-				Console.ReadKey();
-			}
-		}
+        //Changed client type for attempt 2
+        //private DocumentClient client;
+        public static IReliableReadWriteDocumentClient Client { get; set; }
 
-		private async Task GetStartedDemo()
-		{
-			this.client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
-			await this.client.CreateDatabaseIfNotExistsAsync(new Database {Id = "PersonKartotekDB"});
-			await this.client.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri("PersonKartotekDB"),
-				new DocumentCollection {Id = "PersonCollection"});
+        static void Main(string[] args)
+        {
+            IDocumentDbInitializer initializer = new DocumentDbInitializer();
 
-			Address aseAddress = new Address
-			{
-				AddressType = "Work",
-				Street = "Finlandsgade",
-				StreetNumber = "21",
-				ZIP = new ZIP
-				{
-					Country = "Denmark",
-					Town = "Aarhus V",
-					ZipCode = "8210"
-				}
-			};
+            try
+            {
+                Client = initializer.GetClient(EndpointUrl, PrimaryKey);
+                Task program = DemoAsync(args);
+            }
+            catch (DocumentClientException de)
+            {
+                Exception baseException = de.GetBaseException();
+                Console.WriteLine("{0} error occurred: {1}, Message: {2}", de.StatusCode, de.Message,
+                    baseException.Message);
+            }
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit.");
+                Console.ReadKey();
+            }
+        }
 
-			Person nickolaiPerson = new Person
-			{
-				PersonId = "Nickolai.1",
-				FirstName = "Nickolai",
-				MiddleName = "Lundby",
-				LastName = "Ernst",
-				PersonType = "Co-worker",
-				Email = "nicko@minmail.dk",
-				Addresses = new Address[]
-				{
-					new Address
-					{
-						AddressType = "Home",
-						Street = "Vestre Ringgade",
-						StreetNumber = "176",
-						ZIP = new ZIP
-						{
-							Country = "Denmark",
-							Town = "Aarhus C",
-							ZipCode = "8000"
-						}
-					},
-					aseAddress
-				},
-				PhoneNumbers = new PhoneNumber[]
-				{
-					new PhoneNumber
-					{
-						Number = "12345678",
-						PhoneType = "Private",
-						Provider = "TDC"
-					},
-					new PhoneNumber
-					{
-						Number = "87654321",
-						PhoneType = "Work",
-						Provider = "Telia"
-					}
-				}
-			};
+        private static async Task DemoAsync(string[] args)
+        {
+            //Create repository
+            DocumentDbRepository<Person> repository = new DocumentDbRepository<Person>(Client, databaseId);
 
-			await this.CreateIfNotExists("PersonKartotekDB", "PersonCollection", nickolaiPerson);
+            //Create UnitOfWork
+            UnitOfWork UOW = new UnitOfWork(repository);
 
-			Person runePerson = new Person
-			{
-				PersonId = "Rune.1",
-				FirstName = "Rune",
-				MiddleName = "Aagaard",
-				LastName = "Keena",
-				PersonType = "Co-worker",
-				Email = "rune@minmail.dk",
-				Addresses = new Address[]
-				{
-					new Address
-					{
-						AddressType = "Home",
-						Street = "Abildgade",
-						StreetNumber = "21",
-						ZIP = new ZIP
-						{
-							Country = "Denmark",
-							Town = "Aarhus N",
-							ZipCode = "8200"
-						}
-					},
-					aseAddress
-				},
-				PhoneNumbers = new PhoneNumber[]
-				{
-					new PhoneNumber
-					{
-						Number = "12345678",
-						PhoneType = "Private",
-						Provider = "TDC"
-					},
-					new PhoneNumber
-					{
-						Number = "87654321",
-						PhoneType = "Work",
-						Provider = "Telia"
-					}
-				}
-			};
+            //Create zipcodes
+            ZIP Aarhus = new ZIP
+            {
+                Country = "Denmark",
+                Town = "Aarhus",
+                ZipCode = "8000"
+            };
+            ZIP AarhusN = new ZIP
+            {
+                Country = "Denmark",
+                Town = "Aarhus",
+                ZipCode = "8200"
+            };
+            //Create a new person
+            Person person1 = new Person
+            {
+                PersonId = "Nr1",
+                Addresses = new Address[]
+                {
+                    new Address
+                    {
+                        AddressType = "Home",
+                        Street = "Vestre Ringgade",
+                        StreetNumber = "176",
+                        ZIP = Aarhus
+                    },
+                    new Address
+                    {
+                        AddressType = "Work",
+                        Street = "Finlandsgade",
+                        StreetNumber = "17",
+                        ZIP = Aarhus
+                    },
+                },
+                Email = "nickolai@email.dk",
+                FirstName = "Nickolai",
+                LastName = "Ernst",
+                MiddleName = "Lundby",
+                PersonType = "Co-worker",
+                PhoneNumbers = new PhoneNumber[]
+                {
+                    new PhoneNumber
+                    {
+                        Number = "12345678",
+                        PhoneType = "Private",
+                        Provider = "TDC"
+                    },
+                    new PhoneNumber
+                    {
+                        Number = "87654321",
+                        PhoneType = "Work",
+                        Provider = "TDC"
+                    },
+                }
+            };
 
-			await this.CreateIfNotExists("PersonKartotekDB", "PersonCollection", runePerson);
+            //Add created person to database
+            UOW.AddPerson(person1);
 
-			runePerson.MiddleName = "Ole";
+            //Create another person
+            Person person2 = new Person
+            {
+                PersonId = "Nr2",
+                Addresses = new Address[]
+                {
+                    new Address
+                    {
+                        AddressType = "Home",
+                        Street = "Abildgade",
+                        StreetNumber = "21",
+                        ZIP = AarhusN
+                    },
+                    new Address
+                    {
+                        AddressType = "Work",
+                        Street = "Finlandsgade",
+                        StreetNumber = "17",
+                        ZIP = Aarhus
+                    },
+                },
+                Email = "rune@email.dk",
+                FirstName = "Rune",
+                LastName = "Keena",
+                MiddleName = "Aagaard",
+                PersonType = "Co-worker",
+                PhoneNumbers = new PhoneNumber[]
+                {
+                    new PhoneNumber
+                    {
+                        Number = "88888888",
+                        PhoneType = "Private",
+                        Provider = "Telia"
+                    },
+                    new PhoneNumber
+                    {
+                        Number = "22222222",
+                        PhoneType = "Work",
+                        Provider = "TDC"
+                    },
+                }
+            };
 
-			await this.UpdateIfExits("PersonKartotekDB", "PersonCollection", "Rune.1", runePerson);
+            //Add second person to database collection
+            UOW.AddPerson(person2);
+            //Now do the work
+            await UOW.BodyOfWork();
+            //Create operations tested.
+            //Now test update operation
+            person1.FirstName = "Martin";
+            person1.LastName = "Jensen";
+            person2.MiddleName = "Von";
+            await UOW.BodyOfWork();
 
-			await this.RemoveIfExits("PersonKartotekDB", "PersonCollection", "Nickolai.1");
+            //Read operation
+            Person testPerson = new Person();
+            testPerson = await UOW.ReadPerson(person2);
+            Console.WriteLine($"Id of person2: {testPerson.PersonId}");
 
-		}
-
-		private void WriteToConsoleAndPromptToContinue(string format, params object[] args)
-		{
-			Console.WriteLine(format, args);
-			Console.WriteLine("Press any key to continue ...");
-			Console.ReadKey();
-		}
-
-		public class Person
-		{
-			[JsonProperty(PropertyName = "id")] public string PersonId { get; set; }
-			public string FirstName { get; set; }
-			public string MiddleName { get; set; }
-			public string LastName { get; set; }
-			public string PersonType { get; set; }
-			public string Email { get; set; }
-			public Address[] Addresses { get; set; }
-			public PhoneNumber[] PhoneNumbers { get; set; }
-
-			public override string ToString()
-			{
-				return JsonConvert.SerializeObject(this);
-			}
-		}
-
-		public class Address
-		{
-			public string AddressType { get; set; }
-			public string Street { get; set; }
-			public string StreetNumber { get; set; }
-			public ZIP ZIP { get; set; }
-		}
-
-		public class PhoneNumber
-		{
-			public string PhoneType { get; set; }
-			public string Number { get; set; }
-			public string Provider { get; set; }
-		}
-
-		public class ZIP
-		{
-			public string Country { get; set; }
-			public string Town { get; set; }
-			public string ZipCode { get; set; }
-		}
-
-		private async Task CreateIfNotExists(string databaseName, string collectionName, Person person)
-		{
-			try
-			{
-				await this.client.ReadDocumentAsync(UriFactory.CreateDocumentUri(databaseName, collectionName, person.PersonId));
-				this.WriteToConsoleAndPromptToContinue("Found {0}", person.PersonId);
-			}
-			catch (DocumentClientException de)
-			{
-				if (de.StatusCode == HttpStatusCode.NotFound)
-				{
-					await this.client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseName, collectionName),
-						person);
-					this.WriteToConsoleAndPromptToContinue("Created Person {0}", person.PersonId);
-				}
-				else
-				{
-					throw;
-				}
-			}
-		}
-
-		private async Task RemoveIfExits(string databaseName, string collectionName, string documentName)
-		{
-			try
-			{
-				await this.client.DeleteDocumentAsync(UriFactory.CreateDocumentUri(databaseName, collectionName, documentName));
-				this.WriteToConsoleAndPromptToContinue("Person {0} deleted", documentName);
-			}
-			catch (DocumentClientException de)
-			{
-				if (de.StatusCode == HttpStatusCode.NotFound)
-				{
-					this.WriteToConsoleAndPromptToContinue("Person {0} Not Found", documentName);
-				}
-				else
-				{
-					throw;
-				}
-
-			}
-		}
-
-		private async Task UpdateIfExits(string databaseName, string collectionName, string personId, Person updatedPerson)
-		{
-			try
-			{
-				await this.client.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databaseName, collectionName, personId),
-					updatedPerson);
-				this.WriteToConsoleAndPromptToContinue("Replaced Person {0}", personId);
-			}
-			catch (DocumentClientException de)
-			{
-				if (de.StatusCode == HttpStatusCode.NotFound)
-				{
-					this.WriteToConsoleAndPromptToContinue("Person {0} Not Found", personId);
-				}
-				else
-				{
-					throw;
-				}
-			}
-		}
-	}
+            //Delete operation
+            UOW.DeletePerson(person1);
+            await UOW.BodyOfWork();
+        }
+    }
 }
---
+++ I4DAB/Handin3Rune/F18I4DABH2Gr30/F18IDABH2Gr30CosmosDB/UnitOfWork.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentDB.Repository;
+
+namespace F18IDABH2Gr30CosmosDB
+{
+    public class UnitOfWork : IDisposable
+    {
+        //=======================================
+        //Per feedback, now utilizing a repository
+        //https://github.com/Crokus/cosmosdb-repo
+        //========================================
+        private IDocumentDbRepository<Person> _personRepo;
+        private List<Person> _updated = new List<Person>();
+        private List<Person> _removed = new List<Person>();
+        private List<Person> _added = new List<Person>();
+
+        public UnitOfWork(DocumentDbRepository<Person> repository)
+        {
+            _personRepo = repository;
+        }
+        //================
+        // CRUD-Operations
+        //================
+
+        //Create
+        public void AddPerson(Person person)
+        {
+            _added.Add(person);
+            WriteToConsoleAndPromptToContinue($"{person.FirstName} added to the add-queue");
+        }
+
+        //Read
+        public async Task<Person> ReadPerson(Person person)
+        {
+            return await _personRepo.GetByIdAsync(person.PersonId);
+        }
+
+        //Update
+        public void UpdatePerson(Person person)
+        {
+            _updated.Add(person);
+            WriteToConsoleAndPromptToContinue($"{person.FirstName} added to the update-queue");
+        }
+
+        //Delete
+        public void DeletePerson(Person person)
+        {
+            _removed.Add(person);
+            WriteToConsoleAndPromptToContinue($"{person.FirstName} added to the delete-queue");
+        }
+
+        //Work
+        public async Task BodyOfWork()
+        {
+            WriteToConsoleAndPromptToContinue("Working on the DB commencing");
+            //Adding
+            foreach (var added in _added)
+            {
+                await _personRepo.AddOrUpdateAsync(added);
+            }
+            //Updating
+            foreach (var updated in _updated)
+            {
+                await _personRepo.AddOrUpdateAsync(updated);
+            }
+            //Deleting
+            foreach (var removed in _removed)
+            {
+                await _personRepo.RemoveAsync(removed.PersonId);
+            }
+
+            WriteToConsoleAndPromptToContinue("Work on DB complete");
+        }
+
+        private void WriteToConsoleAndPromptToContinue(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+            Console.WriteLine("Press any key to continue ...");
+            Console.ReadKey();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
