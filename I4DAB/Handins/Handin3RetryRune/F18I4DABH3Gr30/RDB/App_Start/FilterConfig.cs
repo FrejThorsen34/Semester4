4b5782856ff1fd01ec3321a50e0cc23bbe9f717f
Rune <runekeena@gmail.com>
2018-04-27 17:59:51 +0200

    Dunno

---
+++ I4DAB/Handins/Handin3RetryRune/F18I4DABH3Gr30/RDB/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RDB
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
