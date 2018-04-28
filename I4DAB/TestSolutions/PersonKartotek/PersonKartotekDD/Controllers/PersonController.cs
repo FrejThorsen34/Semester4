171ceca232605554fbd9467fa4e500399f9e6dc7
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-04-28 14:06:20 +0200

    First attempt at UOW + Controller

---
+++ I4DAB/TestSolutions/PersonKartotek/PersonKartotekDD/Controllers/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PersonKartotekDD.Models;
+
+namespace PersonKartotekDD.Controllers
+{
+    public class PersonController : ApiController
+    {
+        private readonly UnitOfWork UOW = new UnitOfWork();
+        // GET: api/Person
+        public IEnumerable<Person> Get()
+        {
+            return UOW.GetAllPeople();
+        }
+
+        // GET: api/Person/5
+        public Person Get(string id)
+        {
+            return UOW.GetPerson(id);
+        }
+
+        // POST: api/Person
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Person/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Person/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ I4DAB/TestSolutions/PersonKartotek/PersonKartotekDD/Models/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace PersonKartotekDD.Models
+{
+    public class Person
+    {
+        [JsonProperty(PropertyName = "id")] public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string PersonType { get; set; }
+        public string Email { get; set; }
+        public Address[] Addresses { get; set; }
+        public PhoneNumber[] PhoneNumbers { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+    public class Address
+    {
+        [JsonProperty(PropertyName = "id")] public string Id { get; set; }
+        public string AddressType { get; set; }
+        public string Street { get; set; }
+        public string StreetNumber { get; set; }
+        public virtual Zip Zip { get; set; }
+    }
+
+    public class PhoneNumber
+    {
+        [JsonProperty(PropertyName = "id")] public string Id { get; set; }
+        public string PhoneType { get; set; }
+        public string Number { get; set; }
+        public string Provider { get; set; }
+        public virtual Person Person { get; set; }
+    }
+
+    public class Zip
+    {
+        [JsonProperty(PropertyName = "id")] public string Id { get; set; }
+        public string Country { get; set; }
+        public string Town { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
