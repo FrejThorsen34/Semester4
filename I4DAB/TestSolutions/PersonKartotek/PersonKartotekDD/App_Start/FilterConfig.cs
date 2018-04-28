a3e16a8cf304ca871e60f9fe5243866699fa7852
Nickolai Ernst <nickolai.lundby.ernst1@post.au.dk>
2018-04-28 12:49:56 +0200

    Started Rest API for DD

---
+++ I4DAB/TestSolutions/PersonKartotek/PersonKartotekDD/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PersonKartotekDD
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ I4DAB/TestSolutions/PersonKartotek/PersonKartotekDD/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PersonKartotekDD.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
