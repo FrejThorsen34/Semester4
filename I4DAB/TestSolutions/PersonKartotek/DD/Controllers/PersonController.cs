cf87a936dceee1547c632a05a16d73ec8644327e
Rune <runekeena@gmail.com>
2018-04-29 16:05:23 +0200

    Test

---
+++ I4DAB/TestSolutions/PersonKartotek/DD/Controllers/PersonController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+using DD.Models;
+
+namespace DD.Controllers
+{
+    public class PersonController : Controller
+    {
+		// GET: Person
+		[ActionName("Index")]
+		public async Task<ActionResult> IndexAsync()
+		{
+			var people = await DocumentDBRepository<Person>.GetItemsAsync(p => p.Id != null);
+			return View(people);
+		}
+
+	    [ActionName("Create")]
+	    public async Task<ActionResult> CreateAsync()
+	    {
+		    return View();
+	    }
+
+		[HttpPost]
+		[ActionName("Create")]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> CreateAsync([Bind(Include = "Id,FirstName,MiddleName,LastName, Email")] Person person)
+		{
+			if (ModelState.IsValid)
+			{
+				await DocumentDBRepository<Person>.CreateItemAsync(person);
+				return RedirectToAction("Index");
+			}
+
+			return View(person);
+		}
+
+	    [HttpPost]
+	    [ActionName("Edit")]
+	    [ValidateAntiForgeryToken]
+	    public async Task<ActionResult> EditAsync([Bind(Include = "Id,FirstName, MiddleName,LastName,Email")] Person person)
+	    {
+		    if (ModelState.IsValid)
+		    {
+			    await DocumentDBRepository<Person>.UpdateItemAsync(person.Id, person);
+			    return RedirectToAction("Index");
+		    }
+
+		    return View(person);
+	    }
+
+	    [ActionName("Edit")]
+	    public async Task<ActionResult> EditAsync(string id)
+	    {
+		    if (id == null)
+		    {
+			    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+		    }
+
+		    Person person = await DocumentDBRepository<Person>.GetItemAsync(id);
+		    if (person == null)
+		    {
+			    return HttpNotFound();
+		    }
+
+		    return View(person);
+	    }
+	}
+}
---
+++ I4DAB/TestSolutions/PersonKartotek/DD/Models/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+
+namespace DD.Models
+{
+	public class Person
+	{
+		[JsonProperty(PropertyName = "id")] public string Id { get; set; }
+		public string FirstName { get; set; }
+		public string MiddleName { get; set; }
+		public string LastName { get; set; }
+		public string PersonType { get; set; }
+		public string Email { get; set; }
+		public Address[] Addresses { get; set; }
+		public PhoneNumber[] PhoneNumbers { get; set; }
+
+		public override string ToString()
+		{
+			return JsonConvert.SerializeObject(this);
+		}
+	}
+
+	public class Address
+	{
+		public string AddressType { get; set; }
+		public string Street { get; set; }
+		public string StreetNumber { get; set; }
+		public ZIP ZIP { get; set; }
+	}
+
+	public class PhoneNumber
+	{
+		public string PhoneType { get; set; }
+		public string Number { get; set; }
+		public string Provider { get; set; }
+	}
+
+	public class ZIP
+	{
+		public string Country { get; set; }
+		public string Town { get; set; }
+		public string ZipCode { get; set; }
+	}
+}
