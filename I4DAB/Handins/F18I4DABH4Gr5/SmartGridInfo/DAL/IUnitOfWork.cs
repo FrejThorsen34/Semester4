910959903985bc6077b2e8729e590d3d3ac05377
poxbabs <nickolai.ernst@hotmail.com>
2018-05-22 14:42:05 +0200

    SmartGridInfo almost ready

---
+++ I4DAB/Handins/F18I4DABH4Gr5/SmartGridInfo/DAL/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartGridInfo.DAL.Repositories;
+
+namespace SmartGridInfo.DAL
+{
+    public interface IUnitOfWork
+    {
+        ISmartMeterRepository SmartMeterRepository { get; set; }
+        IConnectionRepository ConnectionRepository { get; set; }
+        void Save();
+        Task SaveAsync();
+    }
+}
---
+++ I4DAB/Handins/F18I4DABH4Gr5/SmartGridInfo/DAL/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using SmartGridInfo.DAL.Repositories;
+using SmartGridInfo.Models;
+
+namespace SmartGridInfo.DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public SmartGridInfoContext Context { get; set; }
+        public ISmartMeterRepository SmartMeterRepository { get; set; }
+        public IConnectionRepository ConnectionRepository { get; set; }
+
+        public UnitOfWork(SmartGridInfoContext context)
+        {
+            Context = context;
+            SmartMeterRepository = new SmartMeterRepository(context);
+            ConnectionRepository = new ConnectionRepository(context);
+        }
+        public void Save()
+        {
+            Context.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await Context.SaveChangesAsync();
+        }
+
+    }
+}
