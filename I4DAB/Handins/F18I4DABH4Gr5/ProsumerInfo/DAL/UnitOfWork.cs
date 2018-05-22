d62e8bafc1e025f10d7c0d4cb45d02a22c824cf1
Rune <runekeena@gmail.com>
2018-05-22 11:49:26 +0200

    UOW + Repo

--- I4DAB/Handins/F18I4DABH4Gr5/ProsumerInfo/DAL/UnitOfWork.cs
+++ I4DAB/Handins/F18I4DABH4Gr5/ProsumerInfo/DAL/UnitOfWork.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
+using ProsumerInfo.DAL.Repositories;
 
 namespace ProsumerInfo.DAL
 {
-	public class UnitOfWork
+	public class UnitOfWork : IUnitOfWork
 	{
+		public ProsumerInfoContext Context { get; set; }
+		public IProsumerRepository ProsumerRepository { get; set; }
+		public IdentityRepository IdentityRepository { get; set; }
+
+		public UnitOfWork(ProsumerInfoContext context)
+		{
+			Context = context;
+			ProsumerRepository = new ProsumerRepository(context);
+			IdentityRepository = new IdentityRepository(context);
+		}
+		public void Save()
+		{
+			Context.SaveChanges();
+		}
+
+		public async Task SaveAsync()
+		{
+			await Context.SaveChangesAsync();
+		}
+
 	}
 }
