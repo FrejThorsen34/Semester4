5d072a36ebd2ab7dc3ad2578bb742c27d3f4dd9a
poxbabs <nickolai.ernst@hotmail.com>
2018-05-22 15:06:37 +0200

    Started out the document DB

--- I4DAB/Handins/F18I4DABH4Gr5/TraderInfo/Models/CompletedTrade.cs
+++ I4DAB/Handins/F18I4DABH4Gr5/TraderInfo/Models/CompletedTrade.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
 
 namespace TraderInfo.Models
 {
 	public class CompletedTrade
 	{
-		public string Id { get; set; }
-		public string ProsumerId { get; set; }
-		public string Type { get; set; }
-		public double Tokens { get; set; }
-		public double ExchangeRate { get; set; }
-	}
+	    [JsonProperty(PropertyName = "id", Required = Required.Always)]
+        public string Id { get; set; }
+	    [JsonProperty(PropertyName = "ProsumerId", Required = Required.Always)]
+        public string ProsumerId { get; set; }
+	    [JsonProperty(PropertyName = "Type", Required = Required.Always)]
+        public string Type { get; set; }
+	    [JsonProperty(PropertyName = "Tokens", Required = Required.Always)]
+        public double Tokens { get; set; }
+	    [JsonProperty(PropertyName = "Exchangerate", Required = Required.AllowNull)]
+        public double ExchangeRate { get; set; }
+
+	    public override string ToString()
+	    {
+	        return JsonConvert.SerializeObject(this);
+	    }
+    }
 }
